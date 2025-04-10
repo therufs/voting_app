50df039f9de73dff44cd0a0c92d1f544b5a575e4
Ruthan <rcfreese@gmail.com>
2025-04-10 00:28:44 -0400

    Minor fixes
    Try to get logout working
    Try to get rspec to behave
    Make the votes tally less ugly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
     user = User.find_or_create_by(email: cleaned_email)
     if user
       reset_session
-      session[:email] = user.email
+      # session[:email] = user.email
+      session[:email] = user[:email]
       redirect_to acts_path
     else
       redirect_to login_path, alert: 'Something went wrong.'
@@ -18,6 +19,7 @@ class SessionsController < ApplicationController
   def log_out
     reset_session
     session[:email] = nil
+    redirect_to root_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "home#index" ## TODO send people somewhere useful
+  root "acts#index"
 
   get    '/login',  to: 'sessions#new'
   post   '/login',  to: 'sessions#log_in'
